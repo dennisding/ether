d8d5cccee2d065e7c91e66389fc16bf447c9f30e
dennis <evilangel2000@163.com>
2017-07-21 18:09:49 +0800

    new code

--- entities/components/Login.py
+++ entities/components/Login.py
@@ -17,15 +17,15 @@ def find(doc = {}, **kwds):
 	doc = dict(doc)
 	doc.update(kwds)
 
-	dispatcher = engine.server().dispatcher
+	scheduler = engine.server().scheduler
 
 	# 1. send db request (need a token)
-	# 2. switch out and wait db return(need current)
+	# 2. switch out and wait db return(need current task)
 
-	token = dispatcher.gen_token()
+	token = scheduler.gen_token()
 	db_server.send_db_request(token, doc)
 
-	return dispatcher.wait_for(token)
+	return scheduler.wait_for(token)
 
 def on_db_return(token, doc):
 	dispatcher.satisfy(token, doc)
